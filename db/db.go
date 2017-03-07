d6595e5d8ad5812a5a3f2cad8232a05483769c77
wzdxt <wzdxt0624@gmail.com>
2017-03-07 23:22:32 +0800

    save

--- db/db.go
+++ db/db.go
@@ -12,7 +12,7 @@ var once sync.Once
 func DBInstance() *sql.DB {
 	once.Do(func() {
 		var err error
-		if instance, err = sql.Open("lianjia-mysql", "root:123456@(docker:3306)/lianjia?parseTime=true"); err != nil {
+		if instance, err = sql.Open("mysql", "root:123456@(lianjia-mysql:3306)/lianjia?parseTime=true"); err != nil {
 			panic(err)
 		}
 	})
