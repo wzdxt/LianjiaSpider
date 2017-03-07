baceeecdec716b007360e5b97db7b8324514cd56
wzdxt <wzdxt0624@gmail.com>
2017-03-07 21:57:38 +0800

    save

--- spider/travel.go
+++ spider/travel.go
@@ -65,51 +65,39 @@ func travelXiaoqu(xiaoqu *xiaoqu.Xiaoqu) {
 }
 
 func TravelXiaoquList() {
-	c := make(chan struct{})
-	out:
-	for i := 0; i < 2000; i += 10 {
-		select {
-		case <-c:
-			break out
-		default:
-			go travelXiaoquListRange(i + 1, i + 10, c)
-		}
-		time.Sleep(10 * time.Second)
+	for i := 1; i < 2000; i += 1 {
+		travelXiaoquListRange(i)
+		time.Sleep(3 * time.Second)
 	}
 	log.Println("finish")
 }
 
-func travelXiaoquListRange(l, r int, breakChan chan struct{}) {
-	for i := l; i <= r; i++ {
-		url := fmt.Sprintf("http://sh.lianjia.com/xiaoqu/d%ds13", i)
-		doc := inspector.GetDocFromUrl(url)
+func travelXiaoquListRange(i int) {
+	url := fmt.Sprintf("http://sh.lianjia.com/xiaoqu/d%ds13", i)
+	doc := inspector.GetDocFromUrl(url)
 
-		if doc.Find("ul#house-lst li div.info-panel").Has("h2 a").Each(func(_ int, sel *goquery.Selection) {
-			defer func() {
-				if err := recover(); err != nil {
-					if err, ok := err.(*mysql.MySQLError); ok && err.Number == 1062 {
-					} else {
-						panic(err)
-					}
+	return doc.Find("ul#house-lst li div.info-panel").Has("h2 a").Each(func(_ int, sel *goquery.Selection) {
+		defer func() {
+			if err := recover(); err != nil {
+				if err, ok := err.(*mysql.MySQLError); ok && err.Number == 1062 {
+				} else {
+					panic(err)
 				}
-			}()
-			xiaoquPageId, _ := sel.Find("h2 a").Attr("key")
-			xiaoquName := sel.Find("h2 a").Text()
-			xiaoquNumber64, _ := strconv.ParseInt(sel.Find("div.square span.num").Text(), 10, 64)
-			xiaoquNumber := int(xiaoquNumber64)
-			log.Printf("in page %d found %s", i, xiaoquName)
-			old := xiaoqu_repo.GetByPageId(xiaoquPageId)
-			if (old == nil) {
-				xiaoqu := xiaoqu_repo.Create(xiaoquPageId, xiaoquName, xiaoquNumber)
-				log.Printf("create new xiaoqu %#v", xiaoqu)
-			} else {
-				old.Name, old.Number = xiaoquName, xiaoquNumber
-				xiaoqu_repo.Save(old)
-				log.Printf("udpated xiaoqu %#v", old)
 			}
-		}).Size() == 0 {
-			breakChan <- struct{}{}
-			break;
+		}()
+		xiaoquPageId, _ := sel.Find("h2 a").Attr("key")
+		xiaoquName := sel.Find("h2 a").Text()
+		xiaoquNumber64, _ := strconv.ParseInt(sel.Find("div.square span.num").Text(), 10, 64)
+		xiaoquNumber := int(xiaoquNumber64)
+		log.Printf("in page %d found %s", i, xiaoquName)
+		old := xiaoqu_repo.GetByPageId(xiaoquPageId)
+		if (old == nil) {
+			xiaoqu := xiaoqu_repo.Create(xiaoquPageId, xiaoquName, xiaoquNumber)
+			log.Printf("create new xiaoqu %#v", xiaoqu)
+		} else {
+			old.Name, old.Number = xiaoquName, xiaoquNumber
+			xiaoqu_repo.Save(old)
+			log.Printf("udpated xiaoqu %#v", old)
 		}
-	}
+	}).Size()
 }
