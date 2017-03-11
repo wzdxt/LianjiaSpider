7daa5c826ed5dc328c9cf4d8e8ec8d0fe296e1f7
wzdxt <wzdxt0624@gmail.com>
2017-03-12 00:09:15 +0800

    save

--- main/follow_chengjiao.go
+++ main/follow_chengjiao.go
@@ -3,11 +3,13 @@ package main
 import (
 	"runtime"
 	"github.com/wzdxt/lianjia-spider/spider"
+	"time"
 )
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	spider.FollowChengjiao()
+	time.Sleep(1 * time.Hour)
 }
 
 
